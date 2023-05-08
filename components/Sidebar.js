8ef4c10ba504d6bb083d3475ac38498f6f55ac70
Samuel Suh <samsuh@gmail.com>
2023-05-09 01:55:37 +0900

    add logo

---
+++ components/Sidebar.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function Sidebar() {
+  return (
+    <div>
+      {/* Logo */}
+      <div>
+        <Image src='/twitter-logo.png' width='50' height='50' />
+      </div>
+      {/* Menu */}
+
+      {/* New Tweet Button */}
+
+      {/* Mini Profile */}
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
@@ -9,7 +10,15 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1>Hello World</h1>
+      <main>
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Feed */}
+
+        {/* Widgets */}
+
+        {/* Modal */}
+      </main>
     </div>
   )
 }
