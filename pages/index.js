9b7ca5e2dff25741c40c87ed058d5b7eff961b83
Samuel Suh <samsuh@gmail.com>
2023-05-09 12:42:54 +0900

    create feed section with header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
+        <Feed />
 
         {/* Widgets */}
 
