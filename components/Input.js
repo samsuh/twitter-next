6cd3b7fb45af8d1f7ec48633b4faeae1e8717e9b
Samuel Suh <samsuh@gmail.com>
2023-05-15 04:38:14 +0900

    implement new Post submission to firestore

--- components/Input.js
+++ components/Input.js
@@ -3,14 +3,31 @@ import Image from 'next/image'
 import { useRecoilState } from 'recoil'
 import { userState } from '../atom/userAtom'
 import { getAuth, signOut } from 'firebase/auth'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase'
+import { useState } from 'react'
 
 export default function Input() {
   //use FirebaseAuthentication "currentUser" instead of NextAuth "session.user"
   const [currentUser, setCurrentUser] = useRecoilState(userState)
+  const [input, setInput] = useState('')
+
   const auth = getAuth()
   const onSignout = () => {
     signOut(auth)
     setCurrentUser(null)
+  }
+
+  const sendPost = async () => {
+    const docRef = await addDoc(collection(db, 'posts'), {
+      id: currentUser.uid,
+      text: input,
+      userImg: currentUser.userImg,
+      timestamp: serverTimestamp(),
+      name: currentUser.name,
+      username: currentUser.username,
+    })
+    setInput('')
   }
 
   return (
@@ -30,6 +47,10 @@ export default function Input() {
                 className='w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700'
                 rows='2'
                 placeholder="What's happening?"
+                onChange={(e) => {
+                  setInput(e.target.value)
+                }}
+                value={input}
               ></textarea>
             </div>
             <div className='flex items-center justify-between pt-2.5'>
@@ -37,7 +58,11 @@ export default function Input() {
                 <PhotoIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
                 <FaceSmileIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
               </div>
-              <button className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'>
+              <button
+                disabled={!input.trim()}
+                onClick={sendPost}
+                className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+              >
                 Tweet
               </button>
             </div>
