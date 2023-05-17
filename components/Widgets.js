5129aad74a350e8774f29fa412ed2389854eab84
Samuel Suh <samsuh@gmail.com>
2023-05-17 16:44:18 +0900

    add framer motion animation effect

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,7 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import News from './News'
 import { useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Widgets({ newsResults, randomUserList }) {
   const [articleNum, setArticleNum] = useState(3)
@@ -22,13 +23,23 @@ export default function Widgets({ newsResults, randomUserList }) {
       {/* News Results */}
       <div className='text-gray-700 bg-gray-100 space-y-3 rounded-xl pt-2 w-[90%] xl:w-[75%]'>
         <h4 className='font-bold text-xl px-4'>What's happening</h4>
-        {newsResults.slice(0, articleNum).map((article) => (
-          <News
-            key={article.title}
-            article={article}
-            setArticleNum={setArticleNum}
-          />
-        ))}
+        <AnimatePresence>
+          {newsResults.slice(0, articleNum).map((article) => (
+            <motion.div
+              key={article.title}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <News
+                key={article.title}
+                article={article}
+                setArticleNum={setArticleNum}
+              />
+            </motion.div>
+          ))}
+        </AnimatePresence>
         <button
           className='text-blue-300 pl-4 pb-3 hover:text-blue-400 cursor-pointer'
           onClick={() => setArticleNum(articleNum + 3)}
@@ -40,30 +51,40 @@ export default function Widgets({ newsResults, randomUserList }) {
       {/* Who To Follow  */}
       <div className='sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]'>
         <h4 className='font-bold text-xl px-4'>Who To Follow</h4>
-        {randomUserList.slice(0, followSuggestionCount).map((user) => (
-          <div
-            className='flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200'
-            key={user.login.uuid}
-          >
-            <img
-              src={user.picture.thumbnail}
-              alt='profile picture'
-              width='40'
-              className='rounded-full'
-            />
-            <div className='truncate ml-4 leading-5'>
-              <h4 className='font-bold hover:underline text-[14px]'>
-                {user.login.username}
-              </h4>
-              <h5 className='text-[13px] text-gray-500 truncate'>
-                {user.name.first + ' ' + user.name.last}
-              </h5>
-            </div>
-            <button className='ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold'>
-              Follow
-            </button>
-          </div>
-        ))}
+        <AnimatePresence>
+          {randomUserList.slice(0, followSuggestionCount).map((user) => (
+            <motion.div
+              key={user.login.uuid}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <div
+                className='flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200 transition duration-500 ease-out'
+                key={user.login.uuid}
+              >
+                <img
+                  src={user.picture.thumbnail}
+                  alt='profile picture'
+                  width='40'
+                  className='rounded-full'
+                />
+                <div className='truncate ml-4 leading-5'>
+                  <h4 className='font-bold hover:underline text-[14px]'>
+                    {user.login.username}
+                  </h4>
+                  <h5 className='text-[13px] text-gray-500 truncate'>
+                    {user.name.first + ' ' + user.name.last}
+                  </h5>
+                </div>
+                <button className='ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold'>
+                  Follow
+                </button>
+              </div>
+            </motion.div>
+          ))}
+        </AnimatePresence>
         <button
           className='text-blue-300 pl-4 pb-3 hover:text-blue-400'
           onClick={() => setFollowSuggestionCount(followSuggestionCount + 3)}
